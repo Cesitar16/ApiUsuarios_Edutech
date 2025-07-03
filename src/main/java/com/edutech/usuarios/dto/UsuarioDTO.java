4ed3b778df59b120a6e7a1c274cc61de5b17b331
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-07-02 22:45:30 -0400

    Aplicado Metodo de HATEOAS

--- src/main/java/com/edutech/usuarios/dto/UsuarioDTO.java
+++ src/main/java/com/edutech/usuarios/dto/UsuarioDTO.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class UsuarioDTO {
+public class UsuarioDTO extends RepresentationModel<UsuarioDTO> {
     private Integer id;
     private String nombre;
     private String correo;
